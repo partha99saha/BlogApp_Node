e6b9f55bc85533d6983311ae9b7a932e1a276aba
partha99saha <psaha.bdn9@gmail.com>
2022-04-30 23:46:39 +0530

    update

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use((error, req, res, next) => {
 
 mongoose.connect(MONGODB_URI)
     .then((result) => {
-        console.log("--- connected to MongoDB ---");
+        console.log("--- connected to DB ---");
     })
     .catch((err) => {
         console.log(err);
