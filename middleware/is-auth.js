602c85847fab2cf9f4a5298d271352ab92bfa1cc
partha99saha <psaha.bdn9@gmail.com>
2022-07-04 16:29:08 +0530

    update

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,4 @@
 const jwt = require('jsonwebtoken');
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, './', 'config', '.env') });
 
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization');
