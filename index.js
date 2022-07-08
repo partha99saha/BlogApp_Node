1296d8b4fdf59c36efb0b7fd9f80f4fa39b5033b
partha99saha <psaha.bdn9@gmail.com>
2022-07-08 11:34:41 +0530

    helmet added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const https = require('https');
 
 const express = require('express');
+const helmet = require("helmet");
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer');
@@ -18,30 +19,34 @@ const certificate = fs.readFileSync('server.cert');
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(multer(
-    { storage: fileHandler.fileStorage, fileFilter: fileHandler.fileFilter }).single('image'));
+app.use(multer({
+    storage: fileHandler.fileStorage,
+    fileFilter: fileHandler.fileFilter
+    }).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 
+app.use(helmet());
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
     const data = error.data;
-    res.status(status).json({ message: message, data: data });
+    res.status(status)
+        .json({ message: message, data: data });
 });
 
 const port = bootstrap.port;
 require('./config/dbConfig');
 const server =
-app.listen(port);
-    // https.createServer(
-    //     {
-    //         key: privateKey,
-    //         cert: certificate
-    //     },app).listen(port);
+    app.listen(port);
+// https.createServer(
+//     {
+//         key: privateKey,
+//         cert: certificate
+//     },app).listen(port);
 console.log(`server is running on: ${port}`);
 const io = require('./util/socket').init(server);
 io.on('connection', (socket) => {
