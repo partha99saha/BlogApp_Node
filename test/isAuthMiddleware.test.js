c63dad378e47e4e7a6e04522d292e15be59586b3
partha99saha <psaha.bdn9@gmail.com>
2022-07-10 19:21:38 +0530

    test cases updated

--- test/isAuthMiddleware.test.js
+++ test/isAuthMiddleware.test.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
 const isAuth = require('../middleware/is-auth');
 
 describe('Test Cases for AuthMiddleware', () => {
@@ -10,7 +11,7 @@ describe('Test Cases for AuthMiddleware', () => {
         };
         expect(isAuth.bind(this, req, {}, () => { }))
             .to.throw('Not authenticated.');
-    })
+    });
 
     it('Throw error for Single String', () => {
         const req = {
@@ -20,6 +21,28 @@ describe('Test Cases for AuthMiddleware', () => {
         };
         expect(isAuth.bind(this, req, {}, () => { }))
             .to.throw('jwt must be provided');
-    })
+    });
 
-})
+    it("Throw error for token can't verify", () => {
+        const req = {
+            get: (authHeader) => {
+                return 'Bearer xyz';
+            }
+        };
+        expect(isAuth.bind(this, req, {}, () => { }))
+            .to.throw();
+    });
+
+    it("Should Yield a UserId after decoding the token", () => {
+        const req = {
+            get: (authHeader) => {
+                return 'Bearer xszfbjebjebfjebfjebfjbejfbejbbfbebf';
+            }
+        };
+        jwt.verify = () => {
+            return { userId: 'abc' }
+        };
+        isAuth(req, {}, () => { })
+        expect(req).to.have.property('userId');
+    });
+});
